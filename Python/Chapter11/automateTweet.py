63cd8190a743511bffe2bf4fe276123e7c825d21
Yoshiki Sato <34910699+YoshikiSatoInUEC@users.noreply.github.com>
2019-07-25 12:05:49 +0900

    Update automateTweet.py

--- Python/Chapter11/automateTweet.py
+++ Python/Chapter11/automateTweet.py
@@ -12,7 +12,7 @@ browser.get('https://twitter.com/login')
 uname_elm = browser.find_element_by_class_name('js-username-field')
 uname_elm.send_keys('mlb_yos')
 pass_elm  = browser.find_element_by_class_name('js-password-field')
-pass_elm.send_keys('kershawnaruto')
+pass_elm.send_keys('')
 pass_elm.submit()
 
 #ツイート
